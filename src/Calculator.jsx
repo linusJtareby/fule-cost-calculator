9e73aff28da3be9638fb08b7d6f5dc758de91c4a
“linusJtareby” <linus.tareby@se.experis.com>
2023-10-30 13:57:24 +0100

    some style

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -6,17 +6,23 @@ function CarTripCalculator() {
   const [distance, setDistance] = useState('');
   const [averageUsage, setAverageUsage] = useState('');
   const [totalCost, setTotalCost] = useState(null);
+  const [passengers, setPassengers] = useState('');
+  const [costPerPassenger, setCostPerPassenger] = useState(null)
 
   const calculateTotalCost = () => {
     if (fuelPrice && distance && averageUsage) {
       const cost = ((distance / 10) * averageUsage) * fuelPrice;
+      const costPerPassenger = cost / passengers;
       setTotalCost(cost);
+      setCostPerPassenger(costPerPassenger);
     } else {
       setTotalCost(null);
+      setCostPerPassenger(null);
     }
   };
 
   return (
+    <div className='background'>
     <div className='car-trip-calculator'>
       <h2>Bränslekostnad-räknare</h2>
       <form>
@@ -41,12 +47,23 @@ function CarTripCalculator() {
           />
         </div>
         <div>
-          <label htmlFor="averageUsage">Genomsnittlig förbrukning: (liter/mil): </label>
+          <label htmlFor="averageUsage">Förbrukning: (liter/mil): </label>
           <input
+          placeholder='Ange snittförbrukning...'
             type="number"
             id="averageUsage"
             value={averageUsage}
             onChange={(e) => setAverageUsage(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="passengers">Antal passagerare: </label>
+          <input
+            placeholder='Ange antal passagerare..'
+            type="number"
+            id="distance"
+            value={passengers}
+            onChange={(e) => setPassengers(e.target.value)}
           />
         </div>
         <button type="button" onClick={calculateTotalCost}>
@@ -56,9 +73,14 @@ function CarTripCalculator() {
       {totalCost !== null && (
         <div>
           <h3>Total kostnad för resa: {Math.round(totalCost)} kr</h3>
+          <h3>Kostnad per passagerare: {Math.round(costPerPassenger)} kr</h3>
         </div>
       )}
+
+      <img src='https://s7d1.scene7.com/is/image/hyundai/2023-sonata-hev-ltd-calyspo-red-profile:Vehicle-Carousel?fmt=webp-alpha' width={475} height={160}></img>
     </div>
+    </div>
+
     )
 }
 
